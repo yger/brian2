ba7768da370ecd432ca582ea03284af41af5f7f3
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-11-28 15:37:04 +0100

    Fix an incorrect check for the end of a subgroup in the C++ SpikeQueue.

--- brian2/synapses/cspikequeue.cpp
+++ brian2/synapses/cspikequeue.cpp
@@ -89,7 +89,9 @@ public:
 	void push(int *spikes, unsigned int nspikes)
 	{
 		const unsigned int start = lower_bound(spikes, spikes+nspikes, source_start)-spikes;
-		const unsigned int stop = upper_bound(spikes, spikes+nspikes, source_end)-spikes;
+		// We don't want to include the value source_end itself, therefore we
+		// use source_end-1
+		const unsigned int stop = upper_bound(spikes, spikes+nspikes, source_end-1)-spikes;
 		for(unsigned int idx_spike=start; idx_spike<stop; idx_spike++)
 		{
 			const unsigned int idx_neuron = spikes[idx_spike] - source_start;
