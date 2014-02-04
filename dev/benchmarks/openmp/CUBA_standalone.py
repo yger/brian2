47210d797ae6ced43020d68c842315a1b828db3f
Pierre Yger <pierre@yger.net>
2014-02-04 11:15:13 +0100

    Add benchmark files and fix the coderunner parallelization

---
+++ dev/benchmarks/openmp/CUBA_standalone.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: latin-1
+"""
+CUBA example with delays.
+
+Connection (no delay): 3.5 s
+DelayConnection: 5.7 s
+Synapses (with precomputed offsets): 6.6 s # 6.9 s
+Synapses with weave: 6.4 s
+Synapses with zero delays: 5.2 s
+"""
+
+import sys, time
+from brian2 import *
+
+standalone = int(sys.argv[-2])
+n_threads  = int(sys.argv[-1])
+
+if standalone == 1:
+    set_device('cpp_standalone')
+
+start      = time.time()
+n_threads  = int(sys.argv[-1])
+n_cells    = 1000
+n_exc      = int(0.8*n_cells)
+p_conn     = 0.02
+taum       = 20 * ms
+taue       = 5 * ms
+taui       = 10 * ms
+Vt         = -50 * mV
+Vr         = -60 * mV
+El         = -49 * mV
+
+eqs  = Equations('''
+dv/dt  = (ge+gi-(v-El))/taum : volt
+dge/dt = -ge/taue : volt
+dgi/dt = -gi/taui : volt
+''')
+
+P    = NeuronGroup(n_cells, model=eqs, threshold='v>Vt', reset='v=Vr', refractory=5 * ms)
+P.v  = Vr + rand(len(P)) * (Vt - Vr)
+P.ge = 0 * mV
+P.gi = 0 * mV
+
+Pe   = P[0:n_exc]
+Pi   = P[n_exc:]
+
+we   = (60 * 0.27 / 10)  # excitatory synaptic weight (voltage)
+wi   = (-20 * 4.5 / 10)  # inhibitory synaptic weight
+
+Se   = Synapses(Pe, P, model = 'w : 1', pre = 'ge += w*mV')
+Se.connect('i != j', p=p_conn)
+Se.w     = '%g' %(we)
+Se.delay ='rand()*ms'
+
+Si   = Synapses(Pi, P, model = 'w : 1', pre = 'gi += w*mV')
+Si.connect('i != j', p=p_conn)
+Si.w     = '%g' %(wi)
+Si.delay ='rand()*ms'
+
+spike_mon = SpikeMonitor(P)
+
+print len(P), "neurons in the network"
+print "Simulation running..."
+run(1 * second)
+print time.time()-start
+if standalone == 1:
+    build(project_dir='CUBA_%d' %n_threads, compile_project=True, run_project=True, debug=True, n_threads=n_threads)
