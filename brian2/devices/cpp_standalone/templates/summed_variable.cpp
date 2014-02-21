fe953db953831481c2dd49d224d9812993c62f7f
pyger <pierre.yger@gmail.com>
2014-02-21 15:55:12 +0100

    Faster implementation of the summed_variables, with a local copy. Even if we are looping multiple times over neurons, the main loop over synapses is shared among nodes, and this is the longest one, so the main bottleneck. Therefore, we do see a nice speedup with such an implementation, despite an higher memory consumption

--- brian2/devices/cpp_standalone/templates/summed_variable.cpp
+++ brian2/devices/cpp_standalone/templates/summed_variable.cpp
@@ -5,18 +5,28 @@
     {% set _target_var_array = get_array_name(_target_var) %}
 	//// MAIN CODE ////////////
 	// Set all the target variable values to zero
+	std::vector<double> _local_sum;
+	_local_sum.resize(N_post, 0.0);
+
 	#pragma omp for schedule(static)
 	for (int _target_idx=0; _target_idx<N_post; _target_idx++)
-	    {{_target_var_array}}[_target_idx] = 0.0;
-	
-	#pragma omp single
 	{
-		for(int _idx=0; _idx<_num_synaptic_post; _idx++)
-		{
-			{% for line in code_lines %}
-			{{line}}
-			{% endfor %}
-			{{_target_var_array}}[{{_synaptic_post}}[_idx]] += _synaptic_var;
-		}
+	    {{_target_var_array}}[_target_idx] = 0.0;
 	}
+
+	#pragma omp for schedule(static)
+	for(int _idx=0; _idx<_num_synaptic_post; _idx++)
+	{
+		{% for line in code_lines %}
+		{{line}}
+		{% endfor %}
+		_local_sum[{{_synaptic_post}}[_idx]] += _synaptic_var;
+	}
+
+	for (int _target_idx=0; _target_idx<N_post; _target_idx++)
+	{
+		#pragma omp atomic
+	    {{_target_var_array}}[_target_idx] += _local_sum[_target_idx];	
+	}
+
 {% endblock %}
